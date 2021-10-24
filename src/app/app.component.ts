633ae38a66fa12c7a812f68112b3759046fd2e95
ove <isiful.ovi@gmail.com>
2021-10-24 14:47:55 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {from, Observable, of, pipe} from "rxjs";
+import {flatMap, map, mergeMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-rxjs';
+
+  list: Observable<any> = of(['Ovi', 'Ontu', 'Saiful'])
+  listFrom: Observable<any> = from(['Ovi', 'Ontu', 'Saiful'])
+
+  relList: Observable<any> = of([])
+  relListFrom: Observable<any> = from([])
+
+
+  ngOnInit(): void {
+    /*The from operator emitted one by one from list*/
+    // from(this.list).subscribe(res => {console.log( "FROM : ",res)})
+
+    /*The of operator emitted whole list in one time*/
+    // of(this.list).subscribe(res => {console.log( "FROM : ",res)})
+
+    this.relList = this.list.pipe(map(item => item.map((i: any) => i + " Islam")))
+
+  }
+
+
 }
